7d4e4b45e17cb1627139d102e0ff7483e1be6171
Tobias Kaupat <tobiall@niondir.de>
2017-03-28 16:06:20 +0200

    Add M-Bus checksum (#2)
    Add MBus checksum

--- crc16.go
+++ crc16.go
@@ -16,10 +16,15 @@ const (
 
 	// SCSI is used by SCSI
 	SCSI = 0xEDD1
+
+	MBUS = 0x3D65
 )
 
 // Table is a 256-word table representing the polynomial for efficient processing.
-type Table [256]uint16
+type Table struct {
+	entries  [256]uint16
+	reversed bool
+}
 
 // IBMTable is the table for the IBM polynomial.
 var IBMTable = makeTable(IBM)
@@ -33,14 +38,23 @@ var CCITTFalseTable = makeBitsReversedTable(CCITTFalse)
 // SCSITable is the table for the SCSI polynomial.
 var SCSITable = makeTable(SCSI)
 
+var MBusTable = makeBitsReversedTable(MBUS)
+
 // MakeTable returns the Table constructed from the specified polynomial.
 func MakeTable(poly uint16) *Table {
 	return makeTable(poly)
 }
 
+// MakeTable returns the Table constructed from the specified polynomial.
+func MakeBitsReversedTable(poly uint16) *Table {
+	return makeBitsReversedTable(poly)
+}
+
 // makeTable returns the Table constructed from the specified polynomial.
 func makeBitsReversedTable(poly uint16) *Table {
-	t := new(Table)
+	t := &Table{
+		reversed: true,
+	}
 	width := uint16(16)
 	for i := uint16(0); i < 256; i++ {
 		crc := i << (width - 8)
@@ -51,13 +65,15 @@ func makeBitsReversedTable(poly uint16) *Table {
 				crc <<= 1
 			}
 		}
-		t[i] = crc
+		t.entries[i] = crc
 	}
 	return t
 }
 
 func makeTable(poly uint16) *Table {
-	t := new(Table)
+	t := &Table{
+		reversed: false,
+	}
 	for i := 0; i < 256; i++ {
 		crc := uint16(i)
 		for j := 0; j < 8; j++ {
@@ -67,29 +83,35 @@ func makeTable(poly uint16) *Table {
 				crc >>= 1
 			}
 		}
-		t[i] = crc
+		t.entries[i] = crc
 	}
 	return t
 }
 
 func updateBitsReversed(crc uint16, tab *Table, p []byte) uint16 {
 	for _, v := range p {
-		crc = tab[byte(crc>>8)^v] ^ (crc << 8)
+		crc = tab.entries[byte(crc>>8)^v] ^ (crc << 8)
 	}
 	return crc
 }
 
 func update(crc uint16, tab *Table, p []byte) uint16 {
 	crc = ^crc
+
 	for _, v := range p {
-		crc = tab[byte(crc)^v] ^ (crc >> 8)
+		crc = tab.entries[byte(crc)^v] ^ (crc >> 8)
 	}
+
 	return ^crc
 }
 
 // Update returns the result of adding the bytes in p to the crc.
 func Update(crc uint16, tab *Table, p []byte) uint16 {
-	return update(crc, tab, p)
+	if tab.reversed {
+		return updateBitsReversed(crc, tab, p)
+	} else {
+		return update(crc, tab, p)
+	}
 }
 
 // Checksum returns the CRC-16 checksum of data
@@ -98,17 +120,21 @@ func Checksum(data []byte, tab *Table) uint16 { return Update(0, tab, data) }
 
 // ChecksumIBM returns the CRC-16 checksum of data
 // using the IBM polynomial.
-func ChecksumIBM(data []byte) uint16 { return update(0, IBMTable, data) }
+func ChecksumIBM(data []byte) uint16 { return Update(0, IBMTable, data) }
 
 // ChecksumCCITTFalse returns the CRC-16 checksum using
 // what some call the CCITT-False polynomial, which matches what is used
 // by Perl Digest/CRC and Boost for example.
-func ChecksumCCITTFalse(data []byte) uint16 { return updateBitsReversed(0xffff, CCITTFalseTable, data) }
+func ChecksumCCITTFalse(data []byte) uint16 { return Update(0xffff, CCITTFalseTable, data) }
 
 // ChecksumCCITT returns the CRC-16 checksum of data
 // using the CCITT polynomial.
-func ChecksumCCITT(data []byte) uint16 { return update(0, CCITTTable, data) }
+func ChecksumCCITT(data []byte) uint16 { return Update(0, CCITTTable, data) }
 
 // ChecksumSCSI returns the CRC-16 checksum of data
 // using the SCSI polynomial.
-func ChecksumSCSI(data []byte) uint16 { return update(0, SCSITable, data) }
+func ChecksumSCSI(data []byte) uint16 { return Update(0, SCSITable, data) }
+
+// ChecksumMBus returns the CRC-16 checksum of data
+// using the MBus polynomial.
+func ChecksumMBus(data []byte) uint16 { return Update(0, MBusTable, data) }
