96a97a1abb579c7ff1a8ffa77f2e72d1c314b57f
Chris Howey <howeyc@gmail.com>
2017-07-24 12:55:25 -0500

    Add test for issue #5

--- crc16_test.go
+++ crc16_test.go
@@ -11,10 +11,23 @@ type testCase struct {
 	CRC     uint16
 }
 
+func TestARC(t *testing.T) {
+	tests := []testCase{
+		{[]byte("123456789"), 0xBB3D}}
+	table := MakeTable(IBM)
+	for _, testcase := range tests {
+		result := ^Update(0xFFFF, table, testcase.Message)
+		if testcase.CRC != result {
+			t.Fatalf("ARC CRC-16 value is incorrect, expected %x, received %x.", testcase.CRC, result)
+		}
+	}
+}
+
 func TestModbus(t *testing.T) {
 	tests := []testCase{
 		{[]byte{0xEA, 0x03, 0x00, 0x00, 0x00, 0x64}, 0x3A53},
 		{[]byte{0x4B, 0x03, 0x00, 0x2C, 0x00, 0x37}, 0xBFCB},
+		{[]byte("123456789"), 0x4B37},
 		{[]byte{0x0D, 0x01, 0x00, 0x62, 0x00, 0x33}, 0x0DDD}}
 	for _, testcase := range tests {
 		result := ^ChecksumIBM(testcase.Message)
@@ -32,7 +45,7 @@ func BenchmarkChecksumIBM(b *testing.B) {
 
 func BenchmarkMakeTable(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		MakeTable(0xA001)
+		MakeTable(IBM)
 	}
 }
 
